a7e93681387b5fe9c340756ebf0f787c81831970
Alex Barcelo <alex.barcelo@americanpg.com>
2017-01-09 23:59:35 -0600

    Added Base Weapon. Started converting states over. Started implementing melee. Setup animation notifies. Setup Animation blending.

---
+++ Source/Hauberk/Private/BaseWeapon.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Hauberk.h"
+#include "BaseWeapon.h"
+
+FCombo ABaseWeapon::GetCurrentCombo() const
+{
+	return LastKnownCombo;
+}
+
+FCombo ABaseWeapon::GetNextCombo(EAttackType AttackType)
+{
+	FCombo Combo;
+	switch (AttackType)
+	{
+		case EAttackType::LightAttack:
+			Combo = LightAttacks[ComboIndex];
+			ComboIndex++;
+
+			if (ComboIndex >= LightAttacks.Num())
+			{
+				ComboIndex = 0;
+			}
+
+			LastKnownCombo = Combo;
+			break;
+
+		case EAttackType::ChargeAttack:
+			Combo = ChargeAttacks[ComboIndex];
+			ComboIndex++;
+
+			if (ComboIndex >= ChargeAttacks.Num())
+			{
+				ComboIndex = 0;
+			}
+
+			LastKnownCombo = Combo;
+			break;
+	}
+
+	return Combo;
+}
+
+void ABaseWeapon::ResetCombo()
+{
+	ComboIndex = 0;
+	ClearDamagedActors();
+}
+
+void ABaseWeapon::ClearDamagedActors()
+{
+	if (DamagedActors.Num() > 0)
+	{
+		DamagedActors.Empty();
+	}
+}
